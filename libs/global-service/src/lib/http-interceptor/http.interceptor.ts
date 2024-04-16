adb41a79afaff1ed618ef7d6e9dc306914063ec3
Jackson Tsai <jacksontsai@JacksondeMacBook-Pro.local>
2024-04-16 10:50:50 +0800

    fix: modify auth store and user service

--- libs/global-service/src/lib/http-interceptor/http.interceptor.ts
+++ libs/global-service/src/lib/http-interceptor/http.interceptor.ts
@@ -2,25 +2,36 @@ import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from
 import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import { IAuthState } from '@neo-edge-web/models';
+import { Store, createFeatureSelector, createSelector } from '@ngrx/store';
+import { catchError, switchMap, take, throwError } from 'rxjs';
+
+export const selectAuthState = createFeatureSelector<IAuthState>('auth');
+
+export const selectAccessToken = createSelector(selectAuthState, (state) => state.accessToken);
 
 export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const router = inject(Router);
   const snackBar = inject(MatSnackBar);
-  const authToken = '';
-  const authReq = req.clone({
-    headers: req.headers.set('Authorization', authToken)
-  });
-  return next(authReq).pipe(
-    catchError((error: HttpErrorResponse) => {
-      if (error && (error.status === 401 || error.status === 403)) {
-        router.navigate(['/login']);
-        snackBar.open('"warn": "Login token has expired. Please login again.', 'CLOSE', {
-          horizontalPosition: 'end',
-          verticalPosition: 'bottom'
-        });
-      }
-      return throwError(() => error);
+  const globalStore = inject(Store);
+  return globalStore.select(selectAccessToken).pipe(
+    take(1),
+    switchMap((accessToken) => {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${accessToken}`)
+      });
+      return next(authReq).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error && (error.status === 401 || error.status === 403)) {
+            router.navigate(['/login']);
+            snackBar.open('Login token has expired. Please login again.', 'CLOSE', {
+              horizontalPosition: 'end',
+              verticalPosition: 'bottom'
+            });
+          }
+          return throwError(() => error);
+        })
+      );
     })
   );
 };
