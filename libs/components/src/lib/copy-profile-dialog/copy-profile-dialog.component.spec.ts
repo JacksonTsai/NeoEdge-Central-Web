39b6cc21fa6160f179c90696383f1c61203308d4
Jackson Tsai <jacksontsai@JacksondeMacBook-Pro.local>
2024-06-12 08:07:25 +0800

    feat(copy-dialog): add copy dialog for ot and it page

---
+++ libs/components/src/lib/copy-profile-dialog/copy-profile-dialog.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DeleteOtDeviceConfirmDialogComponent } from './copy-profile-dialog.component';
+
+describe('DeleteOtDeviceConfirmDialogComponent', () => {
+  let component: DeleteOtDeviceConfirmDialogComponent;
+  let fixture: ComponentFixture<DeleteOtDeviceConfirmDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeleteOtDeviceConfirmDialogComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DeleteOtDeviceConfirmDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
