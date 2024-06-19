dbcaaa5eb281b0daa71467e0475dfd09c91d9e2c
Reginna Chao <reginna.chao@ecloudvalley.com>
2024-06-19 15:19:39 +0800

    feat(it service): add file size & format check function

--- libs/components/src/lib/ne-upload-file/ne-upload-file.component.ts
+++ libs/components/src/lib/ne-upload-file/ne-upload-file.component.ts
@@ -1,8 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostListener, computed, forwardRef, input, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  computed,
+  forwardRef,
+  inject,
+  input,
+  signal
+} from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { IItServiceCaFile } from '@neo-edge-web/models';
 
 @Component({
@@ -21,17 +31,37 @@ import { IItServiceCaFile } from '@neo-edge-web/models';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NeUploadFileComponent implements ControlValueAccessor {
-  public sizeMax = input<string | null>(null);
+  #snackBar = inject(MatSnackBar);
+  public maxSize = input<string | null>(null);
   public accept = input<string | null>(null);
   public isDisabled = signal(false);
   public fileInfo = signal<IItServiceCaFile | null>(null);
-  public acceptText = computed<string>(() => {
-    if (!this.accept()) return '';
-    const arr = this.accept()
+
+  private acceptArr = computed<string[]>(() => {
+    if (!this.accept()) return [];
+    return this.accept()
       .split(',')
       .map((ext) => ext.slice(1));
+  });
+  public acceptText = computed<string>(() => {
+    if (!this.accept()) return '';
+    const arr = this.acceptArr();
     const result = arr.slice(0, -1).join(', ') + ' and ' + arr[arr.length - 1];
     return result;
+  });
+
+  private parseSize = computed<number | null>(() => {
+    if (!this.maxSize) return null;
+    const units = { B: 1, KB: 1024, MB: 1048576, GB: 1073741824 };
+    const match = this.maxSize()
+      .toUpperCase()
+      .match(/^(\d+(?:\.\d+)?)\s*(B|KB|MB|GB)$/);
+    if (match) {
+      const value = parseFloat(match[1]);
+      const unit = match[2] as keyof typeof units;
+      return value * units[unit];
+    }
+    return null;
   });
 
   private onChange: (value: IItServiceCaFile) => void;
@@ -40,6 +70,13 @@ export class NeUploadFileComponent implements ControlValueAccessor {
   @HostListener('change', ['$event.target.files']) emitFiles(event: FileList) {
     if (!event) return;
     const file = event && event.item(0);
+
+    // Check file format & Check file size
+    if (!this.checkFileFormat(file) || !this.checkFileSize(file)) {
+      this.onChange(null);
+      return;
+    }
+
     this.setFormValue(file, '');
     const reader = new FileReader();
     reader.onload = (e) => {
@@ -47,6 +84,33 @@ export class NeUploadFileComponent implements ControlValueAccessor {
       this.onTouched();
     };
     reader.readAsText(file);
+  }
+
+  private checkFileFormat(file: File): boolean {
+    if (!this.acceptArr().length) return true;
+    const fileExtension = file?.name.split('.').pop()?.toLowerCase() || '';
+    const result = !!this.acceptArr().includes(fileExtension);
+    if (!result) {
+      this.#snackBar.open(`File type ${fileExtension} is not accepted.`, 'X', {
+        horizontalPosition: 'end',
+        verticalPosition: 'bottom',
+        duration: 5000
+      });
+    }
+    return result;
+  }
+
+  private checkFileSize(file: File): boolean {
+    if (!this.parseSize()) return true;
+    const result = file.size <= this.parseSize();
+    if (!result) {
+      this.#snackBar.open(`File size exceeds the maximum limit.`, 'X', {
+        horizontalPosition: 'end',
+        verticalPosition: 'bottom',
+        duration: 5000
+      });
+    }
+    return result;
   }
 
   private setFormValue(file: File, content: string | ArrayBuffer): void {
