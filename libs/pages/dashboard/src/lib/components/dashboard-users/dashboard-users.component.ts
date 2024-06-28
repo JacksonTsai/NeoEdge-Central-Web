c282daebdd914913733a6cf0e7b5838526acc1ff
Reginna Chao <reginna.chao@ecloudvalley.com>
2024-06-28 09:23:13 +0800

    feat(dashboard): modify use list

--- libs/pages/dashboard/src/lib/components/dashboard-users/dashboard-users.component.ts
+++ libs/pages/dashboard/src/lib/components/dashboard-users/dashboard-users.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { IProjectByIdResp, User } from '@neo-edge-web/models';
 import { FormatCountPipe } from '@neo-edge-web/pipes';
+
+interface IUsersGroupItem {
+  name: string;
+  list: User[];
+}
+
+type IUsersGroup = Record<string, IUsersGroupItem>;
 
 @Component({
   selector: 'ne-dashboard-users',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule, FormatCountPipe],
+  imports: [CommonModule, MatExpansionModule, MatIconModule, FormatCountPipe],
   templateUrl: './dashboard-users.component.html',
   styleUrl: './dashboard-users.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -16,9 +24,28 @@ export class DashboardUsersComponent {
   projectDetail = input<IProjectByIdResp>(null);
   usersList = input<User[]>([]);
 
-  currentUsersList = computed<User[]>(() => {
+  userFilter = computed<User[]>(() => {
     if (!this.projectDetail() || !this.usersList().length) return [];
-    const usersArr = this.projectDetail().users;
-    return this.usersList().filter((user) => usersArr.includes(user.id));
+    return this.usersList().filter((user) => this.projectDetail().users.includes(user.id));
+  });
+
+  currentUsersList = computed<IUsersGroup>(() => {
+    if (!this.userFilter().length) return null;
+
+    const result: IUsersGroup = {};
+
+    this.userFilter().forEach((user) => {
+      const key = user.roleName;
+      if (!result[key]) {
+        result[key] = {
+          name: key,
+          list: []
+        };
+      }
+
+      result[key]?.list.push(user);
+    });
+
+    return result;
   });
 }
