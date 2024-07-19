01b71a4a9b199606acc0c82c473e158b764d6ea9
Reginna Chao <reginna.chao@ecloudvalley.com>
2024-07-19 14:32:59 +0800

    feat(license): finish purchased records

--- libs/components/src/lib/ne-expansion-table/ne-expansion-table.component.ts
+++ libs/components/src/lib/ne-expansion-table/ne-expansion-table.component.ts
@@ -21,7 +21,7 @@ import { MatColumnDef, MatTable, MatTableModule } from '@angular/material/table'
   selector: 'ne-expansion-table',
   standalone: true,
   imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
-  // templateUrl: './ne-expansion-table.component.html',
+  templateUrl: './ne-expansion-table.component.html',
   styleUrl: './ne-expansion-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
@@ -30,59 +30,17 @@ import { MatColumnDef, MatTable, MatTableModule } from '@angular/material/table'
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ])
-  ],
-  template: `
-    <table mat-table [dataSource]="dataSource" multiTemplateDataRows class="mat-elevation-z8">
-      <ng-content></ng-content>
-
-      <ng-container matColumnDef="expand">
-        <th mat-header-cell *matHeaderCellDef aria-label="row actions">&nbsp;</th>
-        <td mat-cell *matCellDef="let element">
-          <button
-            mat-icon-button
-            aria-label="expand row"
-            (click)="expandedElement = expandedElement === element ? null : element; $event.stopPropagation()"
-          >
-            @if (expandedElement === element) {
-              <mat-icon svgIcon="icon:keyboard_arrow_up"></mat-icon>
-            } @else {
-              <mat-icon svgIcon="icon:expand-more"></mat-icon>
-            }
-          </button>
-        </td>
-      </ng-container>
-
-      <!-- Expanded Content Column - The detail row is made up of this one column that spans across all columns -->
-      <ng-container matColumnDef="expandedDetail">
-        <td mat-cell *matCellDef="let element" [attr.colspan]="displayedColumnsWithExpand().length">
-          <div class="example-element-detail" [@detailExpand]="element === expandedElement ? 'expanded' : 'collapsed'">
-            <ng-template [ngTemplateOutlet]="template" [ngTemplateOutletContext]="{ $implicit: element }">
-            </ng-template>
-          </div>
-        </td>
-      </ng-container>
-
-      <!-- Custom row definitions to be provided to the wrapper table. -->
-      <tr mat-header-row *matHeaderRowDef="displayedColumnsWithExpand()"></tr>
-      <tr
-        mat-row
-        *matRowDef="let element; columns: displayedColumnsWithExpand()"
-        class="example-element-row"
-        [class.example-expanded-row]="expandedElement === element"
-        (click)="expandedElement = expandedElement === element ? null : element"
-      ></tr>
-      <tr mat-row *matRowDef="let row; columns: ['expandedDetail']" class="example-detail-row"></tr>
-
-      <!-- Row shown when there is no matching data that will be provided to the wrapper table. -->
-      <tr class="mat-row" *matNoDataRow>
-        <td class="mat-cell" colspan="4">No data</td>
-      </tr>
-    </table>
-  `
+  ]
 })
 export class NeExpansionTableComponent<T> implements AfterContentInit {
   @ContentChildren(MatColumnDef) columnDefs: QueryList<MatColumnDef>;
-  @ContentChild(TemplateRef) template: TemplateRef<any>;
+
+  /* add the ne-template with #detail that can show the detail, like this:
+    <ng-template let-data #detail>
+      <div>{{ data | json }}</div>
+    </ng-template>
+   */
+  @ContentChild('detail') template: TemplateRef<any>;
 
   @ViewChild(MatTable, { static: true }) table: MatTable<T>;
 
--- libs/pages/license/src/lib/components/license-orders/license-orders.component.ts
+++ libs/pages/license/src/lib/components/license-orders/license-orders.component.ts
@@ -1,52 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { NeExpansionTableComponent } from '@neo-edge-web/components';
 import { ICompanyOrder } from '@neo-edge-web/models';
+import { LicenseOrderDetailComponent } from '../license-order-detail/license-order-detail.component';
 
 @Component({
   selector: 'ne-license-orders',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatExpansionModule, NeExpansionTableComponent],
-  // templateUrl: './license-orders.component.html',
-  template: `
-    <ne-expansion-table [dataSource]="companyOrders()" [columns]="displayedColumns">
-      <!-- Custom column definition to be provided to the wrapper table. -->
-      <ng-container matColumnDef="name">
-        <th mat-header-cell *matHeaderCellDef>Name</th>
-        <td mat-cell *matCellDef="let element">{{ element.name }}</td>
-      </ng-container>
-
-      <!-- Position Column -->
-      <ng-container matColumnDef="position">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header>No.</th>
-        <td mat-cell *matCellDef="let element">{{ element.position }}</td>
-      </ng-container>
-
-      <!-- Weight Column -->
-      <ng-container matColumnDef="weight">
-        <th mat-header-cell *matHeaderCellDef mat-sort-header>Weight</th>
-        <td mat-cell *matCellDef="let element">{{ element.weight }}</td>
-      </ng-container>
-
-      <!-- Color Column -->
-      <ng-container matColumnDef="symbol">
-        <th mat-header-cell *matHeaderCellDef>Symbol</th>
-        <td mat-cell *matCellDef="let element">{{ element.symbol }}</td>
-      </ng-container>
-
-      <ng-template let-data>
-        <p>123456789</p>
-        <div>{{ data | json }}</div>
-      </ng-template>
-    </ne-expansion-table>
-  `,
+  imports: [CommonModule, MatTableModule, MatExpansionModule, NeExpansionTableComponent, LicenseOrderDetailComponent],
+  templateUrl: './license-orders.component.html',
   styleUrl: './license-orders.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LicenseOrdersComponent {
   companyOrders = input<ICompanyOrder[]>([]);
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<any>(this.companyOrders());
+  displayedColumns: string[] = ['no', 'orderNumber', 'orderDate', 'customerContact', 'ecvContact'];
+  dataSource = new MatTableDataSource<any>();
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.companyOrders();
+    });
+  }
 }
