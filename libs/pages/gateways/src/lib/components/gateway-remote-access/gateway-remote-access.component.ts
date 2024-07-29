283027794d4a662c8c546847fc8335bc8f31a6e7
Reginna Chao <reginna.chao@ecloudvalley.com>
2024-07-29 16:29:26 +0800

    feat(gateway ssh): url add gateway name & gateway img params

--- libs/pages/gateways/src/lib/components/gateway-remote-access/gateway-remote-access.component.ts
+++ libs/pages/gateways/src/lib/components/gateway-remote-access/gateway-remote-access.component.ts
@@ -13,7 +13,13 @@ import { FormBuilder, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { GATEWAY_LOADING, GATEWAY_SSH_STATUS, IGatewaySSHStatus } from '@neo-edge-web/models';
+import {
+  BOOLEAN_STATUS,
+  GATEWAY_LOADING,
+  GATEWAY_SSH_STATUS,
+  IGatewaySSHStatus,
+  TGatewayStatusInfo
+} from '@neo-edge-web/models';
 
 @Component({
   selector: 'ne-gateway-remote-access',
@@ -26,6 +32,7 @@ import { GATEWAY_LOADING, GATEWAY_SSH_STATUS, IGatewaySSHStatus } from '@neo-edg
 export class GatewayRemoteAccessComponent {
   @Output() handleSwitchSSHMode = new EventEmitter<GATEWAY_SSH_STATUS>();
 
+  gatewayStatusInfo = input<TGatewayStatusInfo | null>(null);
   isConnected = input(false);
   isLoading = input<GATEWAY_LOADING>(GATEWAY_LOADING.NONE);
   sshStatus = input<IGatewaySSHStatus>(null);
@@ -34,6 +41,38 @@ export class GatewayRemoteAccessComponent {
 
   isWaiting = computed<boolean>(() => {
     return this.isLoading() === GATEWAY_LOADING.CONNECT_SSH || this.isLoading() === GATEWAY_LOADING.REFRESH_SSH;
+  });
+
+  logo = computed(() => {
+    if (!this.gatewayStatusInfo()) {
+      return '';
+    }
+
+    if (this.gatewayStatusInfo()?.gatewayIconPath) {
+      return `${this.gatewayStatusInfo().gatewayIconPath}`;
+    }
+
+    if (!this.gatewayStatusInfo().ipcVendorName || !this.gatewayStatusInfo().ipcModelName) {
+      return '/assets/images/default_gateway.png';
+    }
+
+    if (this.gatewayStatusInfo().isPartnerIpc === BOOLEAN_STATUS.TRUE) {
+      return `/assets/images/default_${this.gatewayStatusInfo().ipcVendorName.toLowerCase()}_${this.gatewayStatusInfo().ipcModelName.replace(/\s+/g, '_')}.png`;
+    } else {
+      return '/assets/images/default_gateway.png';
+    }
+  });
+
+  sshUrl = computed<string>(() => {
+    if (!this.sshStatus() || !this.gatewayStatusInfo()) return '';
+
+    const params = new URLSearchParams();
+    const domain = window.location.origin;
+    params.append('token', this.sshStatus()?.current?.token);
+    params.append('gatewayName', encodeURIComponent(this.gatewayStatusInfo().name));
+    params.append('gatewayImg', encodeURIComponent(`${domain}${this.logo()}`));
+
+    return `${this.sshStatus()?.current?.sshServer}?${params.toString()}`;
   });
 
   get isEnabledCtrl() {
